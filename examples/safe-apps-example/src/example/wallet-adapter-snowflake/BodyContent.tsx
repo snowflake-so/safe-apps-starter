648209341acf3604ad451596ba1d30a049238439
Chung Quan Tin <tin@snowflake.so>
2022-10-15 23:18:19 +0700

    Add more methods example for wallet adapter

--- examples/safe-apps-example/src/example/wallet-adapter-snowflake/BodyContent.tsx
+++ examples/safe-apps-example/src/example/wallet-adapter-snowflake/BodyContent.tsx
@@ -2,9 +2,24 @@ import React from 'react';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { Button } from 'antd';
-import { PublicKey, Transaction } from '@solana/web3.js';
+import { Connection, PublicKey, Transaction, TransactionInstruction } from '@solana/web3.js';
 
 type Props = {};
+
+const makeTxn = async (
+  instructions: TransactionInstruction[],
+  feePayer: PublicKey
+): Promise<Transaction> => {
+  const connection = new Connection('https://api.mainnet-beta.solana.com');
+  let transaction = new Transaction();
+  instructions.forEach(instruction => transaction.add(instruction));
+  const latestBlockhash = await connection.getLatestBlockhash('finalized');
+  transaction.recentBlockhash = latestBlockhash.blockhash;
+  transaction.lastValidBlockHeight = latestBlockhash.lastValidBlockHeight;
+  transaction.feePayer = feePayer;
+
+  return transaction;
+};
 
 export const instructions = [
   {
@@ -22,17 +37,46 @@ export const instructions = [
 
 const BodyContent = (props: Props) => {
   const wallet = useWallet();
-  const handleCreateProposal = () => {
-    const transaction = new Transaction();
-    transaction.instructions = instructions;
-    wallet.sendTransaction(
-      transaction,
+  const handleCreateProposal = async () => {
+    const _transaction = await makeTxn(instructions, wallet.publicKey as any);
+    const txId = await wallet.sendTransaction(
+      _transaction,
       null as any,
       {
         name: 'Mock proposal',
       } as any
     );
+
+    console.log(txId);
   };
+
+  const handleSignTransaction = async () => {
+    const _transaction = await makeTxn(instructions, wallet.publicKey as any);
+    if (wallet.signTransaction) {
+      const transaction = await wallet.signTransaction(_transaction);
+      console.log(transaction);
+    }
+  };
+
+  const handleSignMessage = async () => {
+    if (wallet.signMessage) {
+      const message = await wallet.signMessage(Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]));
+      console.log(message);
+    }
+  };
+
+  const handleSignAllTransactions = async () => {
+    const _transaction = await makeTxn(instructions, wallet.publicKey as any);
+    if (wallet.signAllTransactions) {
+      const transaction = await wallet.signAllTransactions([
+        _transaction,
+        _transaction,
+        _transaction,
+      ]);
+      console.log(transaction);
+    }
+  };
+
   return (
     <div className="App">
       <div
@@ -47,6 +91,15 @@ const BodyContent = (props: Props) => {
         <div>
           <Button type="primary" size="large" onClick={handleCreateProposal}>
             Create a mock proposal
+          </Button>
+          <Button type="primary" size="large" onClick={handleSignTransaction}>
+            Sign a transaction
+          </Button>
+          <Button type="primary" size="large" onClick={handleSignMessage}>
+            Sign a message
+          </Button>
+          <Button type="primary" size="large" onClick={handleSignAllTransactions}>
+            Sign all transactions
           </Button>
         </div>
       </div>
